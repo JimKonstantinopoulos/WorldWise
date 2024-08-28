4eed2bb8704573e94af8295cc66c4a0296cc0ddf
dimKonstantinopoulos <dimitris.konstantinopoulos@expressdigibooks.com>
2024-08-28 11:52:15 +0300

    Display cities on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
+import { useCities, flagEmojiToPNG } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
+
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      Map
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{flagEmojiToPNG(city.emoji)}</span>{" "}
+              <span>{city.cityName}</span>{" "}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
